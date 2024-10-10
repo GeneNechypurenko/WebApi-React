505e65a23510806d22be53922f2f862d64765f6a
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2024-10-10 12:15:38 +0300

    #6 Создан класс MessageRepository
    содержит методы работы с данными для таблицы Messages

---
+++ WebApi-React/Repositories/MessageRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi_React.Data;
+using WebApi_React.Models;
+
+namespace WebApi_React.Repositories
+{
+    public class MessageRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MessageRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Message>> GetMessagesAsync()
+        {
+            return await _context.Messages.OrderByDescending(m => m.PostedAt).ToListAsync();
+        }
+
+        public async Task AddMessageAsync(Message message)
+        {
+            _context.Messages.Add(message);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
