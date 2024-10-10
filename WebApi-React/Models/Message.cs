8edaa2bd5cbe4cfe991a893ec54ee8d6f1bdb843
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2024-10-10 11:54:07 +0300

    #2 Создание модели Message
    Модель хранит данные отзывов

---
+++ WebApi-React/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi_React.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Text { get; set; }
+        public DateTime PostedAt { get; set; }
+    }
+}
