039416f31b3046f27db5999b255b1926a8480d58
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2024-10-10 12:27:20 +0300

    #7 Создание класса UserController
    отвечает на запросы клиента связанные с данными из таблицы Users

---
+++ WebApi-React/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi_React.Repositories;
+
+namespace WebApi_React.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly MessageRepository _messageRepository;
+
+        public UserController(MessageRepository messageRepository) => _messageRepository = messageRepository;
+    }
+}
