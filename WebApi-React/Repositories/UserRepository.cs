4a07798a84a3f6c7023026e3e5ccac9e84c63007
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2024-10-10 12:13:47 +0300

    #5 Создан класс UserRepository
    содержит методы добавления данных в таблицу Users

---
+++ WebApi-React/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi_React.Data;
+using WebApi_React.Models;
+
+namespace WebApi_React.Repositories
+{
+    public class UserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetUserByUsernameAsync(string username)
+        {
+            return await _context.Users.SingleOrDefaultAsync(u => u.Username == username);
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
