22765eefae51d459b7406bbe9bfb8891d2ec6ecb
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2024-10-10 11:57:54 +0300

    # 3 Создан класс ApplicationDbContext
    класс ef.core для создания базы данных

---
+++ WebApi-React/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using WebApi_React.Models;
+
+namespace WebApi_React.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
